01afee2394675717586dc8581df1c58d5be62f1a
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-23 23:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   var price=total()
-  if (cardNumber==="") {
+  if (cardNumber===null) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
